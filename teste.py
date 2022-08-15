61ad1c4ca592f8ed098edb21f8e9f15125eec794
Rafael Santos <56464819+SantosSRafael@users.noreply.github.com>
2022-08-15 03:33:43 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,25 @@
+import requests
+import pandas as pd
+
+def retornaPlayers():
+    url = 'https://painel.enxadahost.com/api/client/servers/b0d57748/players'
+    headers = {
+        "Authorization": "Bearer uTko1eqVK088wCuP9rhL9HuDKlqfhGGMosG9HlLk8HEP7VeK",
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+    payload = '{"event":"send stats","args":[null]}'
+
+    response = requests.request('GET', url,  headers=headers)
+    print(response.text)
+    retorno = pd.read_json(response.text)
+
+    retorno  = pd.DataFrame(retorno).transpose()
+
+    # for texto in retorno:
+    #     print(str(texto) + " - " + str(retorno[texto].values[1]))
+    # maxPlayer = retorno["maxPlayers"].values[1]
+    # onlinePlayers = retorno["onlinePlayers"].values[1]
+    # players = retorno["players"].values[1]
+
+    return retorno
